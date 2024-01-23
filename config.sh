a497ba733dc5764fff18e19dff87781d8736a040
PMM4 <73328872+manu06202579@users.noreply.github.com>
2024-01-23 15:46:50 -0300

    Update config.sh
    Galaxy A24

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash env
 
 # about the custom recovery
-export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export NAME="pbrp" # supported: pbrp, shrp, twrp
+export MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb.git" # the link of manifest
+export BRANCH="android-12.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
+export DEVICE="a24" # codename used in device tree
+export DT_LINK="https://github.com/manu06202579/android_device_samsung_a24-pbrp" # device tree link
+export DT_BRANCH="master" # device tree branch
 export VENDOR="samsung" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
